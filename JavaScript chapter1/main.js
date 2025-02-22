edf3951c68f5de2ce1e5a1d49901becc92fa0b60
Abdulrehman Siddiqui <abdulrehmansid7@gmail.com>
2025-02-22 09:57:53 +0500

    Batch15 chapter no 1 alart

---
+++ JavaScript chapter1/main.js
@@ -0,0 +1,34 @@
+let name = "";
+let age = "";
+let course = "";
+let email = "";
+let password = "";
+
+name = alert("welcome to my website");
+name = prompt("please enter your name");
+age = alert("please enter your age");
+age = prompt("please enter your age");
+course = alert("please enter your course");
+course = prompt("course you want to learn");
+email = alert("please enter your email");
+email = prompt("please enter your email");
+password = alert("please enter your password");
+password = prompt("please enter your password");
+
+
+alert("Thanks for provide me detail " + name);
+
+
+console.log(name);
+console.log(age);
+console.log(course);
+console.log(email);
+console.log(password);
+console.log("Thanks for provide me detail " + name);
+
+
+
+
+
+
+
