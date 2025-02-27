5df4e191255cdce5f55f59d561d74ad23687bb8b
Abdulrehman Siddiqui <157453608+Siddiquiweb@users.noreply.github.com>
2025-02-28 01:23:56 +0500

    create a data type
    data type

---
+++ Variables and Data types/main.js
@@ -0,0 +1,35 @@
+let name ="Abdulrehman";     //string
+let age = 20;                //number
+let isMarried = false;        //boolean
+let city = null;              //null
+let country;                  //undefined
+console.table(name, age, isMarried, city, country); //Abdulrehman 20 false null undefined
+
+
+
+
+
+///number
+///string  ""  ''
+///boolean  true false
+//bigInt
+//null empty value
+//undefined    variable is declared but not assigned
+//symbol unique value
+
+
+
+
+
+
+/// how to check data type
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isMarried); //boolean
+console.log(typeof city); //object
+console.log(typeof country); //undefined
+//typeof is used to check data type of variable
+
+
+
+
