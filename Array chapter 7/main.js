9b3d2cbd75ee5ad2b78a444e694f15a6aeca7043
Abdulrehman Siddiqui <157453608+Siddiquiweb@users.noreply.github.com>
2025-03-14 03:33:02 +0500

    advance update

---
+++ Array chapter 7/main.js
@@ -0,0 +1,19 @@
+let a =20;
+let b = 10;
+
+
+let d="26";
+let e=26;
+
+console.log(a > b); // true
+console.log(a < b); // false
+
+console.log(a >= b); // true
+console.log(a <= b); // false
+
+
+
+console.log(d == e); // true
+console.log(d === e); // false
+
+
--- If Else If Statement/main.js
+++ If Else If Statement/main.js
@@ -1,85 +1,215 @@
-let userName="";
-let userAge=10;
-let userCountry="";
-let userCity="";
-let userGender="";
-let userEducation="";
+// let userName="";
+// let userAge=10;
+// let userCountry="";
+// let userCity="";
+// let userGender="";
+// let userEducation="";
 
 
-//get user input
+// //get user input
 
-userName=prompt("Enter your name: ");
-userAge=parseInt(prompt("Enter your age: "));
-userCountry=prompt("Enter your country: ");
-userCity=prompt("Enter your city: ");
-userGender=prompt("Enter your gender: ");
-userEducation=prompt("Enter your education: ");
+// userName=prompt("Enter your name: ");
+// userAge=parseInt(prompt("Enter your age: "));
+// userCountry=prompt("Enter your country: ");
+// userCity=prompt("Enter your city: ");
+// userGender=prompt("Enter your gender: ");
+// userEducation=prompt("Enter your education: ");
 
-//check  if user input is valid
+// //check  if user input is valid
 
-if(userAge>=18){
-  alert("You are an adult");  
-}
-else if(userAge>=13){
-  alert("You are a teenager");
+// if(userAge>=18){
+//   alert("You are an adult");  
+// }
+// else if(userAge>=13){
+//   alert("You are a teenager");
+// }
+// else{
+//   alert("You are a child");
+// }
+
+// if(userCountry==""){
+//   alert("You are a"+userCountry);
+// }
+// else{
+//   alert("You are not a Pakistani");
+// }
+
+// if(userCity==""){
+//   alert("You are from "+userCity);
+// }
+// else{
+//   alert("You are not from Karachi");
+// }
+
+// if(userGender ==""){
+//   alert
+//   ("You are a "+userGender); 
+// }
+// else{
+//   alert("You are a female");
+// }
+// if(userEducation==""){
+//   alert("You are a student ' "+userEducation);
+// }
+// else if(userEducation=="Intermediate"){
+//   alert("You are an Intermediate student");
+// }
+// else if(userEducation=="Bachelors"){
+//   alert("You are a Bachelors student");
+// }
+
+// else{
+//   alert("You are not a student");
+// }
+
+// ///check user console
+
+
+// console.log("Name: "+userName);
+// console.log("Age: "+userAge);
+// console.log("Country: "+userCountry);
+// console.log("City: "+userCity);
+// console.log ("Gender: "+userGender);
+// console.log("Education: "+userEducation);
+
+
+
+// ///check user interface 
+
+// document.write("Name: "+userName+"<br>");
+// document.write("Age: "+userAge+"<br>");
+// document.write("Country: "+userCountry+"<br>");
+// document.write("City: "+userCity+"<br>");
+// document.write("Gender: "+userGender+"<br>");
+// document.write("Education: "+userEducation+"<br>");
+ 
+
+
+
+//customer details 
+
+
+let cutomerName="";
+let customerAge=0;
+let customerCountry="";
+let customerCity="";
+let customerGender="";
+let customerEducation="";
+let customerPhone="";
+let customerEmail="";
+let customerAddress="";
+let customerZipCode="";
+let customerProvince="";
+
+//get customer input
+customerName = prompt("Enter your name");
+customerAge = parseInt(prompt("Enter your age"));
+customerCountry = prompt("Enter your country");
+customerCity = prompt("Enter your city");
+customerGender = prompt("Enter your gender");
+customerEducation = prompt("Enter your education");
+customerPhone = prompt("Enter your phone number");
+customerEmail = prompt("Enter your email");
+customerAddress = prompt("Enter your address");
+customerZipCode = prompt("Enter your zip code");
+customerProvince = prompt("Enter your province");
+//display customer details
+alert("Name: "+customerName);
+alert("Age: "+customerAge);
+alert("Country: "+customerCountry);
+alert("City: "+customerCity);
+alert("Gender: "+customerGender);
+alert("Education: "+customerEducation);
+alert("Phone: "+customerPhone);
+alert("Email: "+customerEmail);
+alert("Address: "+customerAddress);
+alert("Zip Code: "+customerZipCode);
+alert("Province: "+customerProvince);
+
+if(customerAge>=18){
+  alert("you are eligible to admeission");
 }
 else{
-  alert("You are a child");
-}
-
-if(userCountry==""){
-  alert("You are a"+userCountry);
+  alert("you are not eligible to admeission");
+  }
+  Alert("You are a child");
+ if(customerCountry==""){
+  alert("You are a "+customerCountry);
 }
 else{
   alert("You are not a Pakistani");
 }
-
-if(userCity==""){
-  alert("You are from "+userCity);
-}
-else{
-  alert("You are not from Karachi");
+if(customerCity==""){
+  alert("You are from "+customerCity);
+} else {
+  alert("You are not from "+customerCity);
 }
 
-if(userGender ==""){
-  alert
-  ("You are a "+userGender); 
-}
-else{
-  alert("You are a female");
-}
-if(userEducation==""){
-  alert("You are a student ' "+userEducation);
-}
-else if(userEducation=="Intermediate"){
-  alert("You are an Intermediate student");
-}
-else if(userEducation=="Bachelors"){
-  alert("You are a Bachelors student");
-}
-
-else{
-  alert("You are not a student");
-}
-
-///check user console
-
-
-console.log("Name: "+userName);
-console.log("Age: "+userAge);
-console.log("Country: "+userCountry);
-console.log("City: "+userCity);
-console.log ("Gender: "+userGender);
-console.log("Education: "+userEducation);
-
-
-
-///check user interface 
-
-document.write("Name: "+userName+"<br>");
-document.write("Age: "+userAge+"<br>");
-document.write("Country: "+userCountry+"<br>");
-document.write("City: "+userCity+"<br>");
-document.write("Gender: "+userGender+"<br>");
-document.write("Education: "+userEducation+"<br>");
  
+
+// //display customer details in html
+// document.write("Name: "+customerName+"<br>");
+// document.write("Age: "+customerAge+"<br>");
+// document.write("Country: "+customerCountry+"<br>");
+// document.write("City: "+customerCity+"<br>");
+// document.write
+// ("Gender: "+customerGender+"<br>");
+// document.write("Education: "+customerEducation+"<br>");
+// document.write("Phone: "+customerPhone+"<br>");
+// document.write("Email: "+customerEmail+"<br>");
+// document.write("Address: "+customerAddress+"<br>");
+// document.write("Zip Code: "+customerZipCode+"<br>");
+// document.write("Province: "+customerProvince+"<br>");
+
+// //if else if statement
+
+
+
+//display customer details in table
+// document.write("<table border='1'>");
+// document.write("<tr>");
+// document.write("<th>Name</th>");
+// document.write("<th>Age</th>");
+// document.write("<th>Country</th>");
+// document.write("<th>City</th>");
+// document.write
+// ("<th>Gender</th>");
+// document.write("<th>Education</th>");
+// document.write("<th>Phone</th>");
+// document.write("<th>Email</th>");
+// document.write("<th>Address</th>");
+// document.write("<th>Zip Code</th>");
+// document.write("<th>Province</th>");
+// document.write("</tr>");
+// document.write("<tr>"); 
+// document.write("<td>"+customerName+"</td>");
+// document.write("<td>"+customerAge+"</td>");
+// document.write("<td>"+customerCountry+"</td>");
+// document.write("<td>"+customerCity+"</td>");
+// document.write("<td>"+customerGender+"</td>");
+// document.write("<td>"+customerEducation+"</td>");
+// document.write("<td>"+customerPhone+"</td>");
+// document.write("<td>"+customerEmail+"</td>");
+// document.write("<td>"+customerAddress+"</td>");
+// document.write("<td>"+customerZipCode+"</td>");
+// document.write("<td>"+customerProvince+"</td>");
+
+// document.write("</tr>");
+// document.write("</table>");
+// let customerName="";
+// let customerAge=0;
+
+// customerName=prompt("Enter your name: ");
+// customerAge=prompt("Enter your age: ");
+
+// cutomerName=prompt("Enter your name: ");
+// customerAge=prompt("Enter your age: ");
+
+// if(customerAge>=18){
+//   alert("You are an adult");
+//   }else{
+//     alert("You are not an adult");
+  
+//   }
+
+//   // let customerAge=0;
