bfc9fb991a04fdf9bffb73dabe66df51fefd60e7
Abdulrehman Siddiqui <157453608+Siddiquiweb@users.noreply.github.com>
2025-03-19 06:07:05 +0500

    chapter number 8

---
+++ Array chapter 8/main.js
@@ -0,0 +1,49 @@
+const myArr = [1, 2, 3, 4, 5];
+const myHerros=["spiderman","superman","batman","ironman","thor"];
+console.log(0);
+
+
+
+//Arry methods
+//1- push
+//2- pop
+//3- shift
+//4- unshift
+//5- slice
+//6- splice
+//7- concat
+//8- includes
+
+    myArr.push(6);
+    console.log(myArr);
+
+
+// pop is working last element remove
+myArr.pop();
+console.log(myArr);
+
+
+
+///shift is working first element remove
+myArr.shift();
+console.log(myArr);
+
+
+//unshift is working first element add
+myArr.unshift(0);
+console.log(myArr);
+
+
+//slice is working copy of array
+const myArr2 = myArr.slice(1,3);
+console.log(myArr2);
+
+//splice is working add or remove element
+myArr.splice(2,2,6,7);
+console.log(myArr);
+//concat is working add array
+const myArr3 = myArr.concat(myArr2);
+console.log(myArr3);
+//includes is working true or false
+console.log(myArr.includes(5));
+console.log(myArr.includes(9));
